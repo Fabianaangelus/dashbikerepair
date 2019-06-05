faa1937b0c386b35bc04240b4035d6de17f31ec7
Ivan <juniorivandias@gmail.com>
2019-06-05 19:11:15 -0300

    teste

--- static/dist/js/pages/dashboard2.js
+++ static/dist/js/pages/dashboard2.js
@@ -17,7 +17,7 @@ $(function () {
   var salesChart       = new Chart(salesChartCanvas);
 
   var salesChartData = {
-    labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels  : ['OI', 'OI', 'OI', 'OI', 'OI', 'OI', 'OI'],
     datasets: [
       {
         label               : 'Electronics',
@@ -27,17 +27,7 @@ $(function () {
         pointStrokeColor    : '#c1c7d1',
         pointHighlightFill  : '#fff',
         pointHighlightStroke: 'rgb(220,220,220)',
-        data                : [65, 59, 80, 81, 56, 55, 40]
-      },
-      {
-        label               : 'Digital Goods',
-        fillColor           : 'rgba(60,141,188,0.9)',
-        strokeColor         : 'rgba(60,141,188,0.8)',
-        pointColor          : '#3b8bba',
-        pointStrokeColor    : 'rgba(60,141,188,1)',
-        pointHighlightFill  : '#fff',
-        pointHighlightStroke: 'rgba(60,141,188,1)',
-        data                : [28, 48, 40, 19, 86, 27, 90]
+        data                : [500, 1, 80, 81, 56, 55, 40]
       }
     ]
   };
